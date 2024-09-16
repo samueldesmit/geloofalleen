1c717d7aa1f0cfa77aa4198c92d0f25e7ce680a7
samueldesmit <samuel_desmit@hotmail.com>
2024-09-16 23:08:38 +0200

    fix mennu mobile

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,43 +1,42 @@
-import './Nav.scss'
+import './Nav.scss';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Nav() {
   const [menu, setMenu] = useState(false);
-  function handleMenu() {
+
+  const handleMenu = () => {
     setMenu(!menu);
-  }
-  function handleMenuClose() {
+  };
+
+  const handleMenuClose = () => {
     setMenu(false);
-  }
-  let menuClass = menu ? ' navigation__mobile-menu-open' : ' ';
-  let menuButtonClass = menu ? ' harburgermenu-open' : ' ';
+  };
 
   return (
     <div className='outer-container'>
       <header className='navigation'>
-        <h3 className='navigation__home-logo' onClick={handleMenuClose}><NavLink to="/">Geloof Alleen</NavLink></h3>
-        <ul className='navigation__dekstop-menu'>
+        <h3 className='navigation__home-logo' onClick={handleMenuClose}>
+          <NavLink to="/">Geloof Alleen</NavLink>
+        </h3>
+        <ul className='navigation__desktop-menu'>
           <li><NavLink to="/over_mij">Over ons</NavLink></li>
-          {/* <li><NavLink to="/proclamaties">Proclamaties</NavLink></li>  */}
           <li><NavLink to="/contact">Contact</NavLink></li>
-
         </ul>
-        <div className={`harburgermenu${menuButtonClass}`} onClick={handleMenu}>
-          <span className='harburgermenu__burger burger'></span>
-          <span className='harburgermenu__burger burger'></span>
-          <span className='harburgermenu__burger burger'></span>
+        <div className={`harburgermenu ${menu ? 'harburgermenu-open' : ''}`} onClick={handleMenu}>
+          <span className='harburgermenu__burger'></span>
+          <span className='harburgermenu__burger'></span>
+          <span className='harburgermenu__burger'></span>
         </div>
-        <div className={`navigation__mobile-menu${menuClass}`}>
+        <div className={`navigation__mobile-menu ${menu ? 'navigation__mobile-menu-open' : ''}`}>
           <ul className='mobile-navigation-list'>
-            <li onClick={handleMenu}><NavLink to="/over_mij" >Over ons</NavLink></li>
-            {/* <li onClick={handleMenu}><NavLink to="/proclamaties">Proclamaties</NavLink></li> */}
-            <li onClick={handleMenu}><NavLink to="/contact">Contact</NavLink></li>
+            <li onClick={handleMenuClose}><NavLink to="/over_mij">Over ons</NavLink></li>
+            <li onClick={handleMenuClose}><NavLink to="/contact">Contact</NavLink></li>
           </ul>
         </div>
       </header>
     </div>
-  )
+  );
 }
 
-export default Nav
+export default Nav;
